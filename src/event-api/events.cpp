a5cfb80d634b7a758b0c66ef1879f5272c4d3946
ccchanged <changedteamhouse@gmail.com>
2026-06-22 11:40:00 +0500

    Add files via upload

--- src/event-api/events.cpp
+++ src/event-api/events.cpp
@@ -8,19 +8,21 @@ $execute {
     using namespace ffmpeg::events::impl;
 
     FetchVTableEvent().listen([](VTable& vtable, size_t version) {
-        vtable.createRecorder = +[]() -> void* { return new ffmpeg::Recorder; };
-        vtable.deleteRecorder = +[](void* ptr) { delete (ffmpeg::Recorder*)ptr; };
-        vtable.initRecorder = +[](void* ptr, const ffmpeg::RenderSettings& settings) -> Result<> {
-            return ((ffmpeg::Recorder*)ptr)->init(settings);
-        };
-        vtable.stopRecorder = +[](void* ptr) { ((ffmpeg::Recorder*)ptr)->stop(); };
-        vtable.writeFrame = +[](void* ptr, std::span<uint8_t const> frameData) -> Result<> {
-            return ((ffmpeg::Recorder*)ptr)->writeFrame(frameData);
-        };
+        if (version != VTABLE_VERSION)
+            return ListenerResult::Propagate;
 
+        vtable.createRecorder    = +[]() -> void* { return new ffmpeg::Recorder; };
+        vtable.deleteRecorder    = +[](void* ptr) { delete static_cast<ffmpeg::Recorder*>(ptr); };
+        vtable.initRecorder      = +[](void* ptr, const ffmpeg::RenderSettings& settings) -> Result<> {
+            return static_cast<ffmpeg::Recorder*>(ptr)->init(settings);
+        };
+        vtable.stopRecorder      = +[](void* ptr) { static_cast<ffmpeg::Recorder*>(ptr)->stop(); };
+        vtable.writeFrame        = +[](void* ptr, std::span<uint8_t const> frameData) -> Result<> {
+            return static_cast<ffmpeg::Recorder*>(ptr)->writeFrame(frameData);
+        };
         vtable.getAvailableCodecs = &ffmpeg::Recorder::getAvailableCodecs;
-        vtable.mixVideoAudio = &ffmpeg::AudioMixer::mixVideoAudio;
-        vtable.mixVideoRaw = &ffmpeg::AudioMixer::mixVideoRaw;
+        vtable.mixVideoAudio      = &ffmpeg::AudioMixer::mixVideoAudio;
+        vtable.mixVideoRaw        = &ffmpeg::AudioMixer::mixVideoRaw;
 
         return ListenerResult::Stop;
     }).leak();
